ca9dd57237761e7ff7b9c43681e0b6d6eacbe63b
Emily Hunt <emily.hunt.physics@gmail.com>
2022-11-22 10:54:44 +0100

    Merge with GH pages

---
+++ docs/_app/immutable/chunks/2-f287bb9d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-38a2598e.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		/* Not convinced that I need the below? */
+		/*paths: {
+			base: dev ? '' : 'aoc.emilydoesastro.com',
+		},*/
 	}
 };
 
