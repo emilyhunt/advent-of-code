cd28558dae4072a04c5a90fd4bbcc2291e093b96
Emily Hunt <emily.hunt.physics@gmail.com>
2023-12-03 12:33:31 +0100

    Add blank days

---
+++ src/routes/2023/day/25/+page.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+    import * as _info from "./_info.json";
+    export const myMetadata = _info;
+</script>
+
+<script>
+    // Required imports - don't modify!
+    import { currentDefaultData, currentExampleData } from '$lib/stores';
+    import { preprocessData, part1, part2 } from './main';
+    import Runner from "$lib/Runner.svelte";
+
+    // Fetch data
+    import data from "./_data/data.txt?raw";
+    import exampleData from "./_data/exampleData.txt?raw";
+    currentDefaultData.set(preprocessData(data));
+    currentExampleData.set(preprocessData(exampleData));
+
+    // console.log($currentExampleData);
+    // console.log($currentDefaultData);
+</script>
+
+<!-- Component that provides UI to interact with functions defined above. -->
+<Runner part1={part1} part2={part2} preprocessor={preprocessData} metadata={myMetadata}/>
